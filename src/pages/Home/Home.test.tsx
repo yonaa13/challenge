386555b0ad8994f9268522afe076d9bae42e4884
yonaa13 <59670466+yonaa13@users.noreply.github.com>
2023-04-12 12:46:43 -0300

    first commit

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { Home } from "./index";
+describe("login content", () => {
+  test("El login", () => {
+    render(<Home />);
+    //obtener el elemento al que se quiere hacer testing
+    //determinar cual es la prueva a realizar
+  });
+});
---
+++ src/pages/login/Login.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { Login } from "./index";
+describe("login content", () => {
+  test("El login", () => {
+    render(<Login />);
+    //obtener el elemento al que se quiere hacer testing
+    //determinar cual es la prueva a realizar
+  });
+});
